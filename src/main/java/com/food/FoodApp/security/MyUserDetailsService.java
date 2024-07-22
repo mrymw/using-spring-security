ad407f05a3f9c915c0a13c74a0b48dce3dc0af7f
mrymw <maryamtasleem13@gmail.com>
2024-07-22 12:29:29 +0300

    User Details

---
+++ src/main/java/com/food/FoodApp/security/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.food.FoodApp.security;
+
+import com.food.FoodApp.model.User;
+import com.food.FoodApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(username);
+        return new MyUserDetails(user);
+    }
+}
--- src/main/java/com/food/FoodApp/service/UserService.java
+++ src/main/java/com/food/FoodApp/service/UserService.java
@@ -1,7 +1,33 @@
 package com.food.FoodApp.service;
 
+import com.food.FoodApp.exception.InformationNotFoundException;
+import com.food.FoodApp.model.User;
+import com.food.FoodApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder; // encode match - only two methods
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+    @Lazy PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    public User createUser(User userObject) {
+        if(userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw  new InformationNotFoundException("Already Exists");
+        }
+    }
+    public User findUserByEmailAddress(String email) {
+        return userRepository.findUserByEmailAddress(email);
+    }
 }
